6e5c7ffbca8fee8f2dc0b09b2a6f47a2186f264a
Matthew DeMaio <MDemaio@NY.CORP.INT>
2020-08-28 16:33:18 -0400

    Added temp star route

---
+++ client/src/components/StarList.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+   <h1 style="text-align: center;">Stars will go here!</h1>
+</div>
+</template>
+
+<script>
+
+export default {
+    name: "home",
+    data() {
+        return {
+        };
+    },
+    methods: {
+    },
+    mounted() {
+    }
+};
+</script>
+
+<style>
+
+</style>
--- client/src/router.js
+++ client/src/router.js
@@ -7,16 +7,22 @@ export default new Router({ // Add star route.
   mode: "history",
   routes: [
     {
+      path: "/",
+      alias: "/home",
+      name: "home",
+      component: () => import("./components/Home")
+    },
+    {
       path: "/planets",
       alias: "/planets",
       name: "planets-list",
       component: () => import("./components/PlanetList")
     },
     {
-      path: "/",
-      alias: "/home",
-      name: "home",
-      component: () => import("./components/Home")
+      path: "/stars",
+      alias: "/stars",
+      name: "stars-list",
+      component: () => import("./components/StarList")
     }
   ]
 });
