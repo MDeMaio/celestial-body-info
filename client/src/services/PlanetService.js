071834f257209c98e8ac80b62ee9893fa3548869
Matthew DeMaio <36997998+MDeMaio@users.noreply.github.com>
2020-09-10 09:21:57 -0400

    T6 (#13)
    * Small querystring bug fix
    
    * Updated code to dynamically load planet types from DB
    
    * Unfinished adding of Pluto into the data
    
    * Finished pluto data
    
    Co-authored-by: Matthew DeMaio <MDemaio@NY.CORP.INT>

--- client/src/services/PlanetService.js
+++ client/src/services/PlanetService.js
@@ -2,12 +2,15 @@ import http from "../http-common";
 
 class PlanetService {
   getAll(page, type, name) {
-    //return http.get("/planet?page=" + encodeURIComponent(page) + "&type=" + encodeURIComponent(type));
     return http.get(`/planet/${page}/${type}/${name}`);
   }
 
   get(name) {
     return http.get(`/planet/${name}`);
+  }
+
+  getPlanetTypes() {
+    return http.get(`/planettype`);
   }
 
 }
