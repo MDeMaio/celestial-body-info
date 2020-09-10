bc3d9d9fa7d0a2acc262ba2eebd87214b507d40d
Matthew DeMaio <MDemaio@NY.CORP.INT>
2020-09-10 16:00:03 -0400

    Basic start to nasa service

--- generate.sh
+++ generate.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 protoc planet/planetpb/planet.proto --go_out=plugins=grpc:.
+protoc star/starpb/star.proto --go_out=plugins=grpc:.
+protoc nasa/nasapb/nasa.proto --go_out=plugins=grpc:.
