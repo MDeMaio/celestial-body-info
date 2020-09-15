35f81f2f36fe81d804f19c5bbda4592973217d47
dpezella <36972166+dpezella@users.noreply.github.com>
2020-09-14 21:53:09 -0400

    T8 - Footer and Star data (#17)
    * facts
    
    * footer
    
    * footer style
    
    * Small change, always get current year
    
    * Style updates for footer
    
    * Fixed footer overlap content
    
    Co-authored-by: Matthew DeMaio <MDemaio@NY.CORP.INT>

--- client/src/App.vue
+++ client/src/App.vue
@@ -12,11 +12,25 @@
         </div>
     </nav>
 
-    <div class="container-fluid mt-3">
+    <div class="container-fluid mt-3 mb-5">
         <transition name="fade" mode="out-in">
             <router-view />
         </transition>
     </div>
+
+    <footer class="footer fixed-bottom">
+        <div class="container-fluid bg-dark">
+            <div class="row justify-content-md-center" style="font-size:16px;padding-top:20px;">
+                <div class="footer-copyright text-center py-1 text-white">© {{ new Date().getFullYear() }} Copyright</div>
+                <div class="text-right py-1 text-white ml-4">
+                    Maintained by <a href="https://www.linkedin.com/in/matthew-demaio-6848ab15a/" target="_blank">Matthew DeMaio</a> & <a href="https://www.linkedin.com/in/dan-pezella-495897150/" target="_blank">Dan Pezella</a>
+                </div>
+                <div class="text-right py-1 text-white ml-4">
+                    View the code on <a href="https://github.com/MDeMaio/celestial-body-info" target="_blank">Github</a>
+                </div>
+            </div>
+        </div>
+    </footer>
 </div>
 </template>
 
@@ -27,13 +41,23 @@ export default {
 </script>
 
 <style>
+html,
+body {
+    height: 100%;
+}
+
 .app {
     font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
     font-size: 22px;
+    height: 100%;
+}
+
+.footer {
+    font-size: 1px;
 }
 
 html {
-  overflow-y: scroll;
+    overflow-y: scroll;
 }
 
 body{
